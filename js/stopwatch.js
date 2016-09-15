dfbc6848cab14d10c5f60ee55ad2c41fddd61f01
Dmytro96 <DmytroKazaev@gmail.com>
2016-09-15 11:06:41 +0300

    dev-server fix

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,9 +1,9 @@
 require('../css/common.css');
 
-var arrow = document.getElementById('arrow');
+let arrow = document.getElementById('arrow');
 var arrowMinute = document.getElementById('arrowMinute');
 var output = document.getElementById('output');
-var toggleBtn = document.getElementById('toggle');
+let toggleBtn = document.getElementById('toggle');
 var resetBtn = document.getElementById('reset');
 var splitBtn = document.getElementById('split');
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,18 @@ module.exports = {
   devtool: "source-map",
   entry: "./js/stopwatch.js",
   output: {
-    path: __dirname + "/app/dist",
+    path: __dirname + "dist",
     filename: "bundle.js",
   },
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style!css!' },
       { test: /\.png$/, loader: 'url-loader?mimetype=image/png' },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
     ],
   },
   devServer: {
